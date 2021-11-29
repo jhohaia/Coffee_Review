6f560a0bf484f9ecf813b2bc8b470aeb9c28785e
Josh <1934980@ucolcloud.ac.nz>
2021-11-30 12:26:34 +1300

    update/edit coffee review

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -37,3 +37,28 @@ export const updatePost = async (req,res)=>{
 
     res.json(updatedPost) 
 }
+
+export const deletePost = async(req,res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that ID");
+
+    await PostMessage.findByIdAndRemove(id);
+
+    console.log('DELETE')
+
+    res.json({message:'post removed successfully}'});
+}
+
+export const likePost = async (req,res) =>{
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that ID");
+
+    const post = await PostMessage.findById(id);
+    
+    const updatedPost = await PostMessage.findByIdAndUpdate(id,{likeCount : post.likeCount + 1},{new : true})
+
+    res.json(updatedPost);
+
+}
